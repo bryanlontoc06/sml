e75878f38221a2cbd9e22305126523f46ca66c87
Bry <bryanlontoc06@gmail.com>
2023-05-04 23:12:18 +0800

    - connect to Database (MongoDB)

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
-
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
+
+connectDB();
 
 app.use(logger);
 
@@ -26,6 +30,9 @@ app.get('/', (req, res) => {
   ])
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
 })
