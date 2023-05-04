8ec055782f2bcfb88c12b905888281a1f6aface3
Bry <bryanlontoc06@gmail.com>
2023-05-05 00:15:58 +0800

    - add Error Handler

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
@@ -21,6 +22,8 @@ app.use(express.json());
 
 //routes
 app.use('/register', require('./routes/register'));
+
+app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB');
