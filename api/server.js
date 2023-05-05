fe8fad5862b2778dffac19106fe7e44189b0a853
Bry <bryanlontoc06@gmail.com>
2023-05-05 23:54:56 +0800

    - add User API

--- api/server.js
+++ api/server.js
@@ -31,6 +31,8 @@ app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
 
+app.use('/users', require('./routes/api/users'));
+
 app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
