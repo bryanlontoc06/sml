44411e6b69c0a9a5cdeac08680fc5060cf166ce0
Bry <bryanlontoc06@gmail.com>
2023-05-05 23:05:39 +0800

    - add Logout API

---
+++ api/controllers/logoutController.js
@@ -0,0 +1,26 @@
+const User = require('../model/User');
+
+const handleLogout = async (req, res) => {
+    // On client, also delete the access Token
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204); //No content
+    const refreshToken = cookies.jwt;
+
+    // Is refreshToken in db?
+    const foundUser = await User.findOne({ refreshToken }).exec();
+    if (!foundUser) {
+        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+        return res.sendStatus(204);
+    }
+
+    // Delete refreshToken in db
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    console.log(result);
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    res.status(200).json({ 'message': 'User successfully logged out.' }); //No content
+}
+
+
+module.exports = { handleLogout };
